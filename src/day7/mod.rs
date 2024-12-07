536f79592ddfd3151bb164c59a0dc6a4d5b2666b
Lin <hulin2021@gmail.com>
2024-12-07 19:50:46 +0100

    finish day7_2

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,5 +1,6 @@
 use std::cmp::PartialEq;
 use std::collections::{HashMap, HashSet};
+use std::fmt::format;
 
 pub struct Day7;
 
@@ -7,10 +8,11 @@ pub struct Day7;
 enum Operation {
     Add,
     Multiply,
+    Combine
 }
 
 impl Day7 {
-    pub fn first_part(&self, input: &str) -> i64 {
+    fn calculate(&self, input: &str, use_comibine: bool) -> i64 {
         let mut map = HashMap::new();
         input.lines().for_each(|l| {
             let s = l.split(":").collect::<Vec<&str>>();
@@ -27,17 +29,28 @@ impl Day7 {
             let operator_count = v.len() - 1;
             let mut operators = vec![
                 vec![Operation::Add],
-                vec![Operation::Multiply]
+                vec![Operation::Multiply],
             ];
+            if use_comibine {
+                operators.push(vec![Operation::Combine]);
+            }
             for _ in 0..operator_count - 1 {
                 let mut new_operators = vec![];
                 operators.iter().for_each(|op| {
                     let mut new_op = op.clone();
                     new_op.push(Operation::Add);
                     new_operators.push(new_op.clone());
+
                     new_op = op.clone();
                     new_op.push(Operation::Multiply);
                     new_operators.push(new_op.clone());
+
+                    if use_comibine {
+                        new_op = op.clone();
+                        new_op.push(Operation::Combine);
+                        new_operators.push(new_op.clone());
+                    }
+
                 });
                 operators = new_operators;
             }
@@ -48,6 +61,7 @@ impl Day7 {
                     match operator_list[i] {
                         Operation::Add => result += v[i + 1],
                         Operation::Multiply => result *= v[i + 1],
+                        Operation::Combine => result = format!("{}{}", result, v[i + 1]).parse::<i64>().unwrap()
                     }
                 }
                 if result == *k {
@@ -58,8 +72,11 @@ impl Day7 {
         });
         total_result
     }
+    pub fn first_part(&self, input: &str) -> i64 {
+        self.calculate(input, false)
+    }
     pub fn second_part(&self, input: &str) -> i64 {
-        1
+        self.calculate(input, true)
     }
 }
 
@@ -79,11 +96,11 @@ mod tests {
 
     #[test]
     fn second_part_test() {
-        assert_eq!(Day7.second_part(include_str!("day7_input_test.txt")), 6);
+        assert_eq!(Day7.second_part(include_str!("day7_input_test.txt")), 11387);
     }
 
     #[test]
     fn second_part() {
-        assert_eq!(Day7.second_part(include_str!("day7_input.txt")), 6179);
+        assert_eq!(Day7.second_part(include_str!("day7_input.txt")), 424977609625985);
     }
 }
