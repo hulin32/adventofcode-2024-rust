cd1526afeedcb15fc6de0af912eb536a0746d635
Lin <hulin2021@gmail.com>
2024-12-18 23:33:09 +0100

    finish day12_2

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,38 +1,9 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day12;
 
 impl Day12 {
-    fn calculate_price(&self, inputs: Vec<(char, Vec<(i32, i32)>)>) -> i64 {
-        inputs
-            .iter()
-            .map(|(c, v)| {
-                println!("{:?} {:?} {:?}", c, v.len(), v);
-                let perimeter = v
-                    .iter()
-                    .map(|(x, y)| {
-                        // check left, top, right, bottom
-                        let mut count = 0;
-                        if !v.contains(&(x - 1, *y)) {
-                            count += 1;
-                        }
-                        if !v.contains(&(*x, y - 1)) {
-                            count += 1;
-                        }
-                        if !v.contains(&(x + 1, *y)) {
-                            count += 1;
-                        }
-                        if !v.contains(&(*x, y + 1)) {
-                            count += 1;
-                        }
-                        count
-                    })
-                    .sum::<i64>();
-                perimeter * (v.len() as i64)
-            })
-            .sum()
-    }
-    pub fn first_part(&self, input: &str) -> i64 {
+    pub fn filter_data(&self, input: &str) -> Vec<(char, Vec<(i32, i32)>)> {
         let mut data: Vec<(char, Vec<(i32, i32)>)> = vec![];
         let mut points = HashMap::new();
         let mut visited = HashMap::new();
@@ -85,11 +56,105 @@ impl Day12 {
             }
         });
 
-        self.calculate_price(data)
+        data
     }
 
-    pub fn second_part(&self, input: &str) -> usize {
-        1
+    fn first_part(&self, inputs: &str) -> i64 {
+        let data = self.filter_data(inputs);
+        data
+            .iter()
+            .map(|(c, v)| {
+                let perimeter = v
+                    .iter()
+                    .map(|(x, y)| {
+                        // check left, top, right, bottom
+                        let mut count = 0;
+                        if !v.contains(&(x - 1, *y)) {
+                            count += 1;
+                        }
+                        if !v.contains(&(*x, y - 1)) {
+                            count += 1;
+                        }
+                        if !v.contains(&(x + 1, *y)) {
+                            count += 1;
+                        }
+                        if !v.contains(&(*x, y + 1)) {
+                            count += 1;
+                        }
+                        count
+                    })
+                    .sum::<i64>();
+                perimeter * (v.len() as i64)
+            })
+            .sum()
+    }
+
+
+    fn update_edges(&self, edges: &mut HashMap<i32, HashSet<i32>>, key: i32, val: i32) {
+        edges.entry(key).and_modify(|v| {
+            v.insert(val);
+        }).or_insert_with(|| HashSet::from([val]));
+        edges.get_mut(&key).unwrap();
+    }
+    pub fn second_part(&self, input: &str) -> i32 {
+        let data = self.filter_data(input);
+        data
+            .iter()
+            .map(|(c, v)| {
+                let mut x_edges = HashMap::new();
+                let mut y_edges = HashMap::new();
+                v
+                    .iter()
+                    .for_each(|(x, y)| {
+                        // check left, top, right, bottom
+                        if !v.contains(&(x - 1, *y)) {
+                            self.update_edges(&mut y_edges, *x*10 - 4, *y*10);
+                        }
+                        if !v.contains(&(*x, y - 1)) {
+                            self.update_edges(&mut x_edges, *y*10 - 4, *x*10);
+                        }
+                        if !v.contains(&(x + 1, *y)) {
+                            self.update_edges(&mut y_edges, *x*10 + 5, *y*10);
+                        }
+                        if !v.contains(&(*x, y + 1)) {
+                            self.update_edges(&mut x_edges, *y*10 + 5, *x*10);
+                        }
+                    });
+                let mut edge_count = 0;
+                // if (*c == 'B') {
+                //     println!("{:?}", x_edges);
+                //     println!("{:?}", y_edges);
+                // }
+                x_edges.values().for_each(|xs| {
+                    let mut vals: Vec<&i32> = xs.iter().collect();
+                    vals.sort();
+                    let mut current = vals[0] - 10;
+                    edge_count += 1;
+                    for x in vals {
+                        if x - 10 != current {
+                            edge_count += 1;
+                        }
+                        current = *x;
+                    }
+                });
+                y_edges.values().for_each(|ys| {
+                    let mut vals: Vec<&i32> = ys.iter().collect();
+                    vals.sort();
+                    let mut current = vals[0] - 10;
+                    edge_count += 1;
+                    for x in vals {
+                        if x - 10 != current {
+                            edge_count += 1;
+                        }
+                        current = *x;
+                    }
+                });
+
+                println!("{:?}, {:?}, {:?}", c, edge_count, v.len());
+
+                edge_count * v.len() as i32
+            })
+            .sum()
     }
 }
 
@@ -109,7 +174,7 @@ mod tests {
 
     #[test]
     fn second_part_test() {
-        assert_eq!(Day12.second_part(include_str!("day12_input_test.txt")), 1206);
+        assert_eq!(Day12.second_part(include_str!("day12_input_test.txt")), 368);
     }
 
     #[test]
