9325e8dced69b33c92f05e3204657609de972c5f
Lin <hulin2021@gmail.com>
2024-12-20 09:30:56 +0100

    start day13_1

---
+++ src/day13/mod.rs
@@ -0,0 +1,100 @@
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+pub struct Day13;
+
+impl Day13 {
+    pub fn filter_data(&self, input: &str) -> Vec<HashMap<char, (i64, i64)>> {
+        let mut data: Vec<HashMap<char, (i64, i64)>> = vec![];
+        let mut data_item: HashMap<char, (i64, i64)> = HashMap::new();
+        input.lines().for_each(|line| {
+            if line.is_empty() {
+                data.push(data_item.clone());
+                data_item = HashMap::new();
+            } else {
+                let re = Regex::new(r"(\d+)[^0-9]+(\d+)").unwrap();
+                let caps = re.captures(line).unwrap();
+                let x = caps.get(1).unwrap().as_str().parse::<i64>().unwrap();
+                let y = caps.get(2).unwrap().as_str().parse::<i64>().unwrap();
+                if line.contains("Button A") {
+                    data_item.insert('A', (x, y));
+                }
+                if line.contains("Button B") {
+                    data_item.insert('B', (x, y));
+                }
+                if line.contains("Prize:") {
+                    data_item.insert('R', (x, y));
+                }
+            }
+        });
+        data
+    }
+
+    fn first_part(&self, inputs: &str) -> i64 {
+        let data = self.filter_data(inputs);
+        data.iter()
+            .map(|cal| {
+                let (ax, ay) = cal.get(&'A').unwrap();
+                let (bx, by) = cal.get(&'B').unwrap();
+                let (rx, ry) = cal.get(&'R').unwrap();
+                let mut a_count = 0;
+                let mut b_count = 0;
+                let mut found = false;
+                let mut found_result = vec![];
+                loop {
+                    let left_x = rx - a_count * ax;
+                    let left_y = ry - a_count * ay;
+                    if left_x < 0 || a_count > 100 {
+                        break;
+                    }
+
+                    let tmp_b_count = left_x / bx;
+
+                    if left_y == by * tmp_b_count && tmp_b_count <= 100 {
+                        found = true;
+                        b_count = tmp_b_count;
+                        found_result.push(a_count * 3 + b_count);
+                    }
+                    a_count += 1;
+                }
+                if found {
+                    *found_result.iter().min().unwrap()
+                } else {
+                    0
+                }
+            })
+            .sum()
+    }
+
+    pub fn second_part(&self, input: &str) -> i64 {
+        1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day13::Day13;
+
+    #[test]
+    fn first_part_test() {
+        assert_eq!(Day13.first_part(include_str!("day13_input_test.txt")), 480);
+    }
+
+    #[test]
+    fn first_part() {
+        assert_eq!(Day13.first_part(include_str!("day13_input.txt")), 38457);
+    }
+
+    #[test]
+    fn second_part_test() {
+        assert_eq!(
+            Day13.second_part(include_str!("day13_input_test.txt")),
+            1206
+        );
+    }
+
+    #[test]
+    fn second_part() {
+        assert_eq!(Day13.second_part(include_str!("day13_input.txt")), 811148);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,6 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+mod day13;
 
 fn main() {}
